04b5acde84939f2dec32ea92d011dd8a85d71b01
Radiu666 <920659732@qq.com>
2023-10-27 15:54:05 +0800

    打开数据文件以及从数据文件中读取LogRecord

--- data/data_file.go
+++ data/data_file.go
@@ -1,6 +1,16 @@
 package data
 
-import "kv-bitcask/fio"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"kv-bitcask/fio"
+	"path/filepath"
+)
+
+var (
+	ErrInvalidCRC = errors.New("invalid crc")
+)
 
 // DataFile 数据文件，bitcask里面包括的主体，分为active和non-active
 type DataFile struct {
@@ -15,7 +25,17 @@ const (
 
 // OpenDataFile 打开新的数据文件
 func OpenDataFile(dirPath string, fileId uint32) (*DataFile, error) {
-	return nil, nil
+	fileName := filepath.Join(dirPath, fmt.Sprintf("%09d", fileId)+DataFileNameSuffix)
+	// 初始化IO管理器
+	ioManager, err := fio.NewFileIOManager(fileName)
+	if err != nil {
+		return nil, err
+	}
+	return &DataFile{
+		FileId:    fileId,
+		WriteOff:  0,
+		IOManager: ioManager,
+	}, nil
 }
 
 func (df *DataFile) Sync() error {
@@ -31,6 +51,61 @@ func (df *DataFile) Write(buf []byte) error {
 	return nil
 }
 
+// ReadLogRecord 根据给定的offset，读取该位置的LogRecord
 func (df *DataFile) ReadLogRecord(offset int64) (*LogRecord, int64, error) {
-	return nil, -1, nil
+	fileSize, err := df.IOManager.Size()
+	if err != nil {
+		return nil, 0, err
+	}
+
+	// 如果读取的header长度大于文件的大小，则直接读取到文件的结尾即可
+	var headerBytes int64 = maxLogRecordHeaderSize
+	if offset+maxLogRecordHeaderSize > fileSize {
+		headerBytes = fileSize - offset
+	}
+
+	// 读取Header
+	headerBuf, err := df.ReadNBytes(headerBytes, offset)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	header, headerSize := DecodeLogRecordHeader(headerBuf)
+	// 判断头文件是否有效
+	if header == nil {
+		return nil, 0, io.EOF
+	}
+	if header.crc == 0 && header.keySize == 0 && header.valueSize == 0 {
+		return nil, 0, io.EOF
+	}
+
+	// 提取key value的长度
+	keySize, valueSize := int64(header.keySize), int64(header.valueSize)
+	var recordSize = headerSize + keySize + valueSize
+
+	logRecord := &LogRecord{Type: header.recordType}
+	// 读取实际的key value值
+	if keySize > 0 || valueSize > 0 {
+		kvBuf, err := df.ReadNBytes(keySize+valueSize, offset+headerSize)
+		if err != nil {
+			return nil, 0, err
+		}
+		// 得到key value
+		logRecord.Key = kvBuf[:keySize]
+		logRecord.Value = kvBuf[keySize:]
+	}
+
+	// 验证CRC
+	crc := getLogRecordCRC(logRecord, headerBuf)
+	if crc != header.crc {
+		return nil, 0, ErrInvalidCRC
+	}
+	return logRecord, recordSize, nil
+}
+
+// ReadNBytes 读取指定长度的字节数组
+func (df *DataFile) ReadNBytes(n int64, offset int64) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := df.IOManager.Read(b, offset)
+	return b, err
 }
--- data/log_record.go
+++ data/log_record.go
@@ -1,5 +1,7 @@
 package data
 
+import "encoding/binary"
+
 // LogRecordType 描述该行记录是否应被删除
 type LogRecordType = byte
 
@@ -7,6 +9,19 @@ const (
 	LogRecordNormal LogRecordType = iota
 	LogRecordDeleted
 )
+
+// LogRecord头部信息
+// crc	 type	keySize	valueSize
+//
+//	4      1      5         5
+const maxLogRecordHeaderSize = binary.MaxVarintLen32*2 + 5
+
+type LogRecordHeader struct {
+	crc        uint32        // crc校验码
+	recordType LogRecordType // 类型记录
+	keySize    uint32
+	valueSize  uint32
+}
 
 // LogRecordPos 数据内存索引，描述数据在磁盘的位置
 type LogRecordPos struct {
@@ -22,4 +37,12 @@ type LogRecord struct {
 
 func EncodeLogRecord(logRecord *LogRecord) ([]byte, int64) {
 	return nil, -1
+}
+
+func DecodeLogRecordHeader(buf []byte) (*LogRecordHeader, int64) {
+	return nil, -1
+}
+
+func getLogRecordCRC(lr *LogRecord, header []byte) uint32 {
+	return 0
 }
